c60f200cfaefd28ed09529692ff2af7832de5d9a
ILikeDogss <yingting.sun1@gmail.com>
2024-05-24 23:11:26 +0800

    Tail improvememnt

--- Stay Hidden/Assets/Scripts/TailMechanic.cs
+++ Stay Hidden/Assets/Scripts/TailMechanic.cs
@@ -6,8 +6,18 @@ public class TailMechanic : MonoBehaviour
 {
     public GameObject tail;
     public Player_Movement pM;
+    public Camera mainCam;
+    private float cameraZDistance;
+    public Transform tailParent;
+    public Vector3 initialScale;
+
+    private void Start()
+    {
+        cameraZDistance = mainCam.WorldToScreenPoint(transform.position).z;
+    }
     void Update()
     {
+      
         if (Input.GetMouseButton(0))
         {
             tail.SetActive(true);
@@ -15,15 +25,20 @@ public class TailMechanic : MonoBehaviour
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = rotation;
+            Vector3 MouseScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+            Vector3 MouseWorldPosition = mainCam.ScreenToWorldPoint(MouseScreenPosition);
+            float distance = Vector3.Distance(tailParent.position, MouseWorldPosition);
+            transform.localScale = new Vector3(distance / 2f, initialScale.y, initialScale.z);
+            //tail.transform.localScale = new Vector3(Input.mousePosition.x + Input.mousePosition.y, 1f, 1f);
         }
         else
         {
             tail.SetActive(false);
         }
-        Flip();
+        //Flip();
     }
 
-    void Flip()
+    /*void Flip()
     {
         if(pM.isFacingRight == false)
         {
@@ -34,6 +49,6 @@ public class TailMechanic : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-    }
+    }*/
 
 }
