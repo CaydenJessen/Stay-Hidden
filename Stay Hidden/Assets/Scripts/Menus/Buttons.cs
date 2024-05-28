43736031efc552ee12f4af87f7ada2902e0edcd1
AndyLee-SH <163143209+AndyLee-SH@users.noreply.github.com>
2024-05-28 11:16:24 +0800

    fixed pause menu issues

--- Stay Hidden/Assets/Scripts/Menus/Buttons.cs
+++ Stay Hidden/Assets/Scripts/Menus/Buttons.cs
@@ -7,17 +7,46 @@ public class Buttons : MonoBehaviour
 {
     public string sceneName;
     public GameObject pauseMenu;
+    public GameObject[] objects;
+    public bool on = true;
   
     public void LoadScene()
     {
-        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);   
+        Debug.Log("hey i reset");
     }
+
 
     public void Resume()
     {
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
     }
+    public void turnoff()
+    {
+        if(on == true)
+        {
+            for (int i = 0; i < objects.Length; i++)
+                {
+                    objects[i].SetActive(false);
+                    on = false;
+                }
+            
+        }
+        else
+        {
+            for (int i = 0; i < objects.Length; i++)
+                {
+                    objects[i].SetActive(true);
+                    on = true;
+                }
+        }
+
+
+    }
+        
+    
 
 }
 
--- Stay Hidden/Assets/Scripts/Menus/Pause.cs
+++ Stay Hidden/Assets/Scripts/Menus/Pause.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public bool isPaused; 
     // Start is called before the first frame update
-    void Click()
+   /* void Click()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             pauseMenu.SetActive(true);
+            isPaused = true; 
         }
-    }
-
+   }
+*/ 
     // Update is called once per frame
-    void Update()
+     void Update()
     {
-        Click();
+        
+
+         if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("hello");
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true; 
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false; 
+        }
         
     }
 }
