e3c21f59918968132dfda0561f976e9e45d95554
ILikeDogss <yingting.sun1@gmail.com>
2024-06-02 14:09:29 +0800

    last update before submission

--- Stay Hidden/Assets/Scripts/Floating.cs
+++ Stay Hidden/Assets/Scripts/Floating.cs
@@ -1,16 +1,22 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class Floating : MonoBehaviour
 {
-    public float speed = 2f;
-    public float height = 0.2f;
+    public float amp;
+    public float freq;
+    Vector3 initPos;
 
-
-    void Update()
+    private void Start()
     {
-        float newY = Mathf.Sin(Time.time * speed) * height + transform.position.y;
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        initPos = transform.position;
+        transform.position = initPos;
+    }
+
+    private void Update()
+    {
+        
+        transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, 0);
     }
 }
