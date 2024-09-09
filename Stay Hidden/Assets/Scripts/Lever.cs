596e70f31a01133752a047d4a85db71b180735eb
ILikeDogss <yingting.sun1@gmail.com>
2024-09-09 11:00:34 +0800

    Lever animation

--- Stay Hidden/Assets/Scripts/Lever.cs
+++ Stay Hidden/Assets/Scripts/Lever.cs
@@ -6,52 +6,53 @@ public class Lever : MonoBehaviour
 {
     public GameObject[] openMechanisms;
     public GameObject[] closeMechanisms;
-    public GameObject lever;
-    public GameObject leverControl;
+    //public GameObject lever;
+    //public GameObject leverControl;
     public bool inRange = false;
     public bool status = false;
     public int numberOfMechanisms;
     public Player_Movement pM;
-
+    public Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if ((inRange == true) && (status == false))
+            if ((inRange == true) && (status == true))
             {
                 for (int i = 0; i < numberOfMechanisms; i++)
                 {
                     openMechanisms[i].SetActive(false);
                     closeMechanisms[i].SetActive(true);
                 }
-                
-                leverControl.transform.Rotate(0.0f, 0.0f, -41.0f);
 
-                status = true;
+                // leverControl.transform.Rotate(0.0f, 0.0f, -41.0f);
+                animator.SetBool("LeverOn", false);
+                status = false;
+                pM.view.SetActive(true);
             }
-            else if ((inRange == true) && (status == true))
+            else if ((inRange == true) && (status == false))
                 {
                 for (int i = 0; i < numberOfMechanisms; i++)
                 {
                     openMechanisms[i].SetActive(true);
                     closeMechanisms[i].SetActive(false);
                 }
-                leverControl.transform.Rotate(0.0f, 0.0f, 41.0f);
-                    status = false;
-                }
+                //leverControl.transform.Rotate(0.0f, 0.0f, 41.0f);
+                    status = true;
+                animator.SetBool("LeverOn", true);
+                pM.view.SetActive(true);
+            }
+            else
+            {
+                pM.view.SetActive(false);
+            }
          
         }
 
-        if(inRange == true)
-        {
-            lever.GetComponent<Renderer>().material.color = Color.red;
-            pM.viewing = true;
-        }
-        else
-        {
-            lever.GetComponent<Renderer>().material.color = Color.white;
-            pM.viewing = false;
-        }
 
     }
 
