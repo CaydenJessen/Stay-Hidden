861f6c694ef3721f29a0e2455a82ad76071a19a6
ILikeDogss <yingting.sun1@gmail.com>
2024-09-24 12:00:20 +0800

    knife improvement

--- Stay Hidden/Assets/Scripts/FallingKnife.cs
+++ Stay Hidden/Assets/Scripts/FallingKnife.cs
@@ -8,47 +8,76 @@ public class FallingKnife : MonoBehaviour
     BoxCollider2D boxCollider2D;
     public float distance;
     bool isFalling = false;
+    bool isRising = false;
+    Vector2 P;
+    public float shakeAmount = 1.0f; 
+    Vector2 startPosition;
 
-
+        
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
-
+        startPosition.x = transform.position.x;
+        startPosition.y = transform.position.y;
+        P = transform.position;
 
     }
 
     private void Update()
     {
+        if(isRising == true)
+        { 
+            Rising();
+            isRising = false;
+        }
         Physics2D.queriesStartInColliders = false;
-        if(isFalling == false)
+        if (isFalling == false)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down,distance);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance);
 
-            Debug.DrawRay(transform.position,Vector2.down * distance, Color.red);
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
 
-            if(hit.transform != null)
+            if (hit.transform != null)
             {
-                if(hit.transform.tag == "Player")
+                if (hit.transform.tag == "Player")
                 {
-                    rb.gravityScale = 5;
-                    isFalling = true;
+                    StartCoroutine(Falling());
                 }
             }
         }
     }
+
     private void OnCollisionEnter2D(Collision2D other)
      {
-         if(other.gameObject.tag == "Player")
-        {
-             Destroy(gameObject);
-        }
-        else
-        {
-            rb.gravityScale = 0;
-             boxCollider2D.enabled = false;  
-        }
-        
 
+        isRising = true;
+
+    }
+    
+
+    IEnumerator Falling()
+    {
+        transform.position = P;
+        for (int i = 0; i < 10; i++)
+        {
+            P.x = startPosition.x + shakeAmount;
+            yield return new WaitForSeconds(0.1f);
+            P.x = startPosition.x - shakeAmount;
+            P.y = startPosition.y + shakeAmount;
+            yield return new WaitForSeconds(0.1f);
+            P.y = startPosition.y - shakeAmount;
+
+        }
+        rb.gravityScale = 5;
+        isFalling = true;
+    }
+
+    IEnumerator Rising()
+    {
+        yield return new WaitForSeconds(2);
+        transform.position = Vector3.MoveTowards(transform.localPosition, startPosition, 1);
+        rb.gravityScale = 0;
+        isFalling = false;
     }
 }
