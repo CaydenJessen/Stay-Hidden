dcbcd0b62caa15de683b451ee9d21b0e09a513cf
AndyLee-SH <34270331@student.murdoch.edu.au>
2024-09-30 14:25:32 +0800

    Jump fix

---
+++ Stay Hidden/Assets/Scripts/GroundedCollider.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedCollider : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = transform.parent.GetComponent<Animator>();
+        
+    }
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            anim.SetBool("Grounded", true);
+        }
+        
+    }
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Ground")
+        {
+            anim.SetBool("Grounded", false);
+        }
+    }
+}
